c01a60d67a709cc629f60e091be0c121c600497a
iqoopid <ciril3jc@gmail.com>
2021-03-25 12:15:00 +0530

    init

--- area1.py
+++ area1.py
@@ -1,6 +1,6 @@
-a=int(input("enter side 1:"))
-b=int(input("enter side 2:"))
-c=int(input("enter side 3:"))
+a=float(input("enter side 1:"))
+b=float(input("enter side 2:"))
+c=float(input("enter side 3:"))
 s=(a+b+c)/2
 x=s*(s-a)*(s-b)*(s-c)
 A=x**0.5
